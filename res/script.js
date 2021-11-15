f55cae4ac38ffe885ac54e2371fc9cca05b316f8
Daniele Caraglio <daniele.caraglio@gmail.com>
2021-11-15 22:52:24 +0100

    impl: removed jquery; bugfix whitebackground

--- res/script.js
+++ res/script.js
@@ -1,44 +1,48 @@
-var ACE = (function(){
-  var audio = new Audio('/res/slideventura.mp3');
+var UTIL = (function() {
+    let util = {};
+
+    util.minMax = function(value, min, max) {
+      return Math.max(min, Math.min(max, value));
+    }
+
+    util.map = function(x1, x2, y2) {
+      return x1 * y2 / x2;
+    }
+
+    return util;
+})();
+
+var ACE = (function() {
+  let ace = {
+    width: null
+  };
+
+  let audio = new Audio('/res/slideventura.mp3');
   audio.loop = true;
   audio.volume = 0.5;
 
-  var w = window,
-    d = document,
-    e = d.documentElement,
-    g = d.getElementsByTagName('body')[0];
-
   var $text = document.getElementById('text');
-  var width = document.getElementById('img').offsetWidth;
-  var height;
   var currentPosition = 0;
   var targetPosition = 0;
   var backgroundPosition = 0;
   var currentStep = 0;
   var totalStep = 26;
 
-  var ace = {
-    init: function() {
+  let $body, $img;
+
+  ace.init = function() {
+      $body  = document.querySelector('body');
+      $img = document.querySelector('#img');
 
       window.addEventListener('resize', function() {
-        width = document.getElementById('img').offsetWidth;
+        ACE.getWidth();
       }, true);
+      ACE.getWidth();
 
-      $('body').on('mousemove touchmove', function(e) {
-        var currentX;
+      $body.addEventListener('mousemove', ACE.move);
+      $body.addEventListener('touchmove', ACE.move);
 
-        if (!isNaN(e.pageX)) {
-          currentX = e.pageX;
-        } else {
-          var touch = event.targetTouches[event.targetTouches.length-1];
-          currentX = touch.pageX;
-        }
-        currentX = minMax(currentX - $('#img').offset().left, 0, width);
-
-        targetPosition = width - currentX;
-      });
-
-      $('body').on('click', function(e) {
+      $body.addEventListener('click', function() {
         audio.play();
         $text.style.display = 'none';
       });
@@ -48,39 +52,49 @@ var ACE = (function(){
         $text.style.display = 'block';
       }
 
-      function anim(timestamp) {
-
-        currentPosition += (targetPosition - currentPosition) / 6;
-        currentStep = Math.round(map(currentPosition, width, totalStep));
-
-			  backgroundPosition = Math.floor(currentStep, 1) * width;
-
-        $('#img').css('background-position-x', backgroundPosition + 'px');
-
-        if (currentStep > 0 && currentStep <= totalStep) {
-            audio.volume = map(Math.min(Math.max(currentStep - 4, 0), totalStep - 10), totalStep - 10, 0.5);
-        } else {
-          audio.volume = 0;
-        }
-        requestAnimationFrame(anim);
-      }
-      requestAnimationFrame(anim);
-
-      function map(x1, x2, y2) {
-        return x1 * y2 / x2;
-      }
-
-      function minMax(value, min, max) {
-        return Math.max(min, Math.min(max, value));
-      }
-    }
+      requestAnimationFrame(ACE.animation);
   }
 
-  return {
-    init: function() {
-      return ace.init();
-    }
+  ace.getWidth = function() {
+    let rect = document.getElementById('img').getBoundingClientRect();
+    ACE.width = rect.width;
   }
+
+  ace.move = function(e) {
+    let currentX;
+
+    if (!isNaN(e.pageX)) {
+      currentX = e.pageX;
+    } else {
+      let touch = event.targetTouches[event.targetTouches.length-1];
+      currentX = touch.pageX;
+    }
+    let rect = $img.getBoundingClientRect();
+    let offsetLeft = rect.left + window.scrollX;
+    currentX = UTIL.minMax(currentX - offsetLeft, 0, ACE.width);
+
+    targetPosition = ACE.width - currentX;
+    console.log(targetPosition);
+  }
+
+  ace.animation = function() {
+    currentPosition += (targetPosition - currentPosition) / 6;
+    currentStep = Math.round(UTIL.map(currentPosition, ACE.width, totalStep));
+
+    backgroundPosition = Math.max(currentStep, 1) * (ACE.width);
+
+
+    $img.style.backgroundPositionX =  backgroundPosition + 'px';
+
+    if (currentStep > 0 && currentStep <= totalStep) {
+        audio.volume = UTIL.map(Math.min(Math.max(currentStep - 4, 0), totalStep - 10), totalStep - 10, 0.5);
+    } else {
+      audio.volume = 0;
+    }
+    requestAnimationFrame(ACE.animation);
+  }
+
+  return ace;
 })();
 
 ACE.init();
